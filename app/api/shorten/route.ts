c4ebabaa8c5a5aedb4e8f9592926ef826cf9ddbb
Brendan Chess <66980517+brendan-chess@users.noreply.github.com>
2024-02-19 11:44:15 -0600

    database interaction
    creating a new shortened url posts to db. if hash is already store for given link, existing record is returned

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -1,10 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import hash from "fnv1a";
+import prisma from "@/prisma/prisma";
+import { UrlHash } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
   const req: { url: string } = await request.json();
 
-  const value = hash(req.url);
+  const value = hash(req.url).toString(36);
 
-  return NextResponse.json(value.toString(36));
+  let urlHash: UrlHash | null = await prisma.urlHash.findFirst({
+    where: {
+      hash: {
+        equals: value,
+      },
+    },
+  });
+
+  if (urlHash == null) {
+    urlHash = await prisma.urlHash.create({
+      data: {
+        url: req.url,
+        hash: value,
+      },
+    });
+  }
+
+  return NextResponse.json(urlHash.hash);
 }
