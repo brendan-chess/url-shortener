3004856ca43999a5319787de16fd7ede0f6d15f9
Brendan Chess <66980517+brendan-chess@users.noreply.github.com>
2024-02-19 11:26:30 -0600

    prisma setup
    created a mongodb database for the project and setup prisma orm to start connecting with db

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import InputForm from "./components/InputForm";
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-600 to-violet-600 px-8">
-      <div className="flex flex-col w-full max-w-[600px] min-w-[300px] justify-between bg-white p-10 rounded-md shadow-md">
+      <div className="flex flex-col w-full max-w-[600px] min-w-[300px] justify-between bg-white p-8 rounded-md shadow-md">
         <div className="text-2xl font-semibold text-neutral-800">
           URL Shortener
         </div>
